1f47d25bba50dd7647ffaa3a38ccf3817a5516bd
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2023-12-28 00:16:14 +0530

    Spring Security integration

--- myapp/src/main/java/com/test/myapp/rest/restController.java
+++ myapp/src/main/java/com/test/myapp/rest/restController.java
@@ -1,10 +1,17 @@
 package com.test.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class restController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/")
     public String sayHello(){
