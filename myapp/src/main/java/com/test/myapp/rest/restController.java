6ef78d87ee9260b087ace8ffd44e7b1c3aed9a6b
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2023-12-27 23:25:14 +0530

    New Get Mappings Added

--- myapp/src/main/java/com/test/myapp/rest/restController.java
+++ myapp/src/main/java/com/test/myapp/rest/restController.java
@@ -6,10 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class restController {
 
-    //expose a "/" endpoint for hello world
     @GetMapping("/")
     public String sayHello(){
         return "Hello world";
+    }
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
     }
 
 }
